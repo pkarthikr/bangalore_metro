a9faeb6950fe9d93dfdf8aa77eb3a774ba3db26f
Karthik Ragubathy <me@pkarthik.in>
2019-06-25 21:33:04 +0530

    Single Line setup in progress

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -25,30 +25,6 @@ const LaunchRequestHandler = {
   },
 };
 
-// const InProgressFareIntentHandler = {
-//   canHandle(handlerInput) {
-//     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-//       && handlerInput.requestEnvelope.request.intent.name === 'FareIntent'
-//       && handlerInput.requestEnvelope.request.dialogState !== 'COMPLETED'
-//   },
-//   handle(handlerInput) {
-//     handlerInput = handlerInput.requestEnvelope.request.intent;
-//     if(handlerInput.slots.sourceStation.resolutions.resolutionsPerAuthority[0].status.code == "ER_SUCCESS_NO_MATCH"){
-//       console.log("WE did not get a match for this station");
-//     }; 
-
-//     // console.log(stations.green);
-//     // var stationCount = stations.green.filter(x => x.name == stationName);
-//     // if(stationCount == 0){
-//     //   const speechText = "I am sorry, I was not able to find the station.";
-//     //   return handlerInput.responseBuilder
-//     //   .speak(speechText)
-//     //   .reprompt('Here we are')
-//     //   .getResponse();
-//     // }  
-//   }
-// }
-
 const FareIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -58,16 +34,20 @@ const FareIntentHandler = {
     const sourceStation = handlerInput.requestEnvelope.request.intent.slots.sourceStation.resolutions.resolutionsPerAuthority[0].values[0].value.name;
     const destinationStation = handlerInput.requestEnvelope.request.intent.slots.destinationStation.resolutions.resolutionsPerAuthority[0].values[0].value.name;
 
-    // First check if both stations are on the green line. 
-    const sourceGreen = stations.green.filter(x => x.name === sourceStation);
-    const destinationGreen = stations.green.filter(x => x.name === destinationStation);
+    const sourceLine = stations.filter(x => x.name === sourceStation);
+    const destinationLine = stations.filter(x => x.name === destinationStation);
+
     var speechText = `Okay! So you want to go from ${sourceStation} to ${destinationStation}`;
 
-    // If both the stations are on the same line
-    if(sourceGreen.length > 0 && destinationGreen.length > 0){
-      speechText = `Both are on green line`;
+    if(sourceLine[0].line == "green" && destinationLine[0].line == "green"){
+      var sourceOrder = sourceLine[0].order;
+      var destinationOrder = destinationLine[0].order;
+      console.log(sourceOrder);
+      console.log(destinationOrder);
+      const stationsFiltered = stations.filter((x => x.order < sourceOrder && x.order > destinationOrder) || (x => x.order > sourceOrder && x.order < destinationOrder));
+      console.log(stationsFiltered);
+      speechText = `We are on same Line`;
     }
-
 
     return handlerInput.responseBuilder
       .speak(speechText)
