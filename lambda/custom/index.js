3ecfc2f3a535b1e3d78a215d0c808226fa2c39a3
Karthik Ragubathy <me@pkarthik.in>
2019-06-29 23:54:17 +0530

    Fixed issues with stations next to each other

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,27 +3,28 @@
 
 const Alexa = require('ask-sdk-core');
 const stations = require('./stations.json');
+const SKILL_NAME = "Bangalore Metro";
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    
-    const speechText = 'Welcome to Bangalore Metro. I can help you get from one station to the other, and help you with the fare.';
+
+    const speechText = 'Welcome to Bangalore Metro. I can help you with the route from one station to another! Just say your origin and destination station to know your route.';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
       .getResponse();
   },
 };
 
-const FareIntentHandler = {
+const RouteIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'FareIntent';
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'RouteIntent';
   },
   handle(handlerInput) {
     const sourceStation = handlerInput.requestEnvelope.request.intent.slots.sourceStation.resolutions.resolutionsPerAuthority[0].values[0].value.name;
@@ -31,107 +32,142 @@ const FareIntentHandler = {
 
     const sourceLine = stations.filter(x => x.name === sourceStation);
     const destinationLine = stations.filter(x => x.name === destinationStation);
-
+    console.log("Our destination is ");
+    console.log(destinationLine);
     var speechText = `Okay! So to go from ${sourceStation} to ${destinationStation}, you will have to take the`;
+    var sourceOrder = sourceLine[0].order;
+    var destinationOrder = destinationLine[0].order;
+    var majesticGreenOrder = 14;
+    var majesticPurpleOrder = 8;
 
-    if((sourceLine[0].line == "green" && destinationLine[0].line == "green")|| (sourceLine[0].line == "purple" && destinationLine[0].line == "purple" )){
-      var sourceOrder = sourceLine[0].order;
-      var destinationOrder = destinationLine[0].order;
-      
-      var stationsFiltered = stations.filter(x => {if(x.order < sourceOrder && x.order > destinationOrder && x.line == sourceLine[0].line){
-        return true;
-      } if(x.order > sourceOrder && x.order < destinationOrder && x.line == sourceLine[0].line){
-        return true;
-      }
-      return false;
+    if ((sourceLine[0].line == "green" && destinationLine[0].line == "green") || (sourceLine[0].line == "purple" && destinationLine[0].line == "purple")) {
+     
+      console.log("inbside green");
+      console.log(sourceLine);
+      var stationsFiltered = stations.filter(x => {
+        if (x.order < sourceOrder && x.order > destinationOrder && x.line == sourceLine[0].line) {
+          return true;
+        }
+        if (x.order > sourceOrder && x.order < destinationOrder && x.line == sourceLine[0].line) {
+          return true;
+        }
+        if (x.order == destinationOrder && x.line == sourceLine[0].line) {
+          return true;
+        }
+        return false;
       });
       speechText += ` ${sourceLine[0].line} line. Get down ${stationsFiltered.length} stations from ${sourceStation}. ${destinationStation} will come right after`;
 
       // When travelling the opposite direction
-      if(sourceOrder > destinationOrder){
-        stationsFiltered = stationsFiltered.sort(function(a,b){
+      if (sourceOrder > destinationOrder) {
+        stationsFiltered = stationsFiltered.sort(function (a, b) {
           return b.order - a.order;
         });
       }
-
-      speechText += ` ${stationsFiltered[stationsFiltered.length-1].name} station`;
       
-      // stationsFiltered.forEach(function(station){
-      //   speechText += `<break time = "1s"/>${station.name}`
-      // });
-    } else if(sourceLine[0].line == "green" && destinationLine[0].line == "purple"){
-      // When you have to switch lines
-      var sourceOrder = sourceLine[0].order;
-      var destinationOrder = destinationLine[0].order;
-      var majesticGreenOrder = 14;
-      var majesticPurpleOrder = 8;
+      speechText += ` ${stationsFiltered[stationsFiltered.length-1].name} station`;
+
+      if(destinationOrder - sourceOrder == 1 || sourceOrder - destinationOrder == -1){
+        speechText = `On the ${sourceLine[0].line} line, ${sourceStation} and ${destinationStation} are just next to each other`;
+      }
+    } else if (sourceLine[0].line == "green" && destinationLine[0].line == "purple") {
+      // When you have to switch lines from green to purple
       var speechText = `Okay! So to go from ${sourceStation} to ${destinationStation}, you will have to switch from green to purple line. From ${sourceStation}, take the train towards`;
-      if(sourceOrder < 14){
-          speechText += `Yelachenahalli`;
-      } else{
-          speechText += `Nagasandra`;
+      if (sourceOrder < 14) {
+        speechText += `Yelachenahalli`;
+      } else {
+        speechText += `Nagasandra`;
       }
       var distance = Math.abs(sourceOrder - majesticGreenOrder);
       speechText += ` and get down at Majestic which is ${distance - 1} stations away. From Majestic, take the train towards`;
-      if(destinationOrder > 8){
+      var destinationCalculation = 0;
+      if (destinationOrder > 8) {
         speechText += 'Baipaynahalli';
+        destinationCalculation = destinationOrder - 1;
+      } else {
+        speechText += 'Mysore Road';
+        destinationCalculation = destinationOrder + 1;
+      }
+      var destinationDistance = Math.abs(destinationOrder - majesticPurpleOrder);
+      speechText += ` and get down ${destinationDistance - 1} stations later.`;
+      var stationsFiltered = stations.filter(x => {
+        if (x.order == destinationCalculation && x.line == destinationLine[0].line) {
+          return true;
+        }
+        return false;
+      });
+     
+      if(stationsFiltered.length > 0){
+        speechText += ` ${destinationStation} will come right after. ${stationsFiltered[0].name} station`;
+      }
+
+    } else if (sourceLine[0].line == "purple" && destinationLine[0].line == "green"){
+      var speechText = `Okay! So to go from ${sourceStation} to ${destinationStation}, you will have to switch from purple to green line. From ${sourceStation}, take the train towards`;
+      if (sourceOrder < 8){
+        speechText += 'Baiyappanahalli';
       } else {
         speechText += 'Mysore Road';
       }
-      var destinationDistance = Math.abs(destinationOrder - majesticPurpleOrder);
-      speechText += ` and get down ${destinationDistance - 1} stations later. ${destinationStation} will come right after`;
-      var stationsFiltered = stations.filter(x => {if(x.order == destinationOrder - 1 && x.line == destinationLine[0].line){
-        return true;
-      } 
-      return false;
+      var distance = Math.abs(sourceOrder - majesticPurpleOrder);
+      speechText += ` and get down at Majestic which is ${distance - 1} stations away. From Majestic, take the train towards`;
+      var destinationCalculation = 0;
+      if (destinationOrder > 14) {
+        speechText += 'Yelachenahalli';
+        destinationCalculation = destinationOrder - 1;
+      } else {
+        speechText += 'Nagasandra';
+        destinationCalculation = destinationOrder + 1
+      }
+      var destinationDistance = Math.abs(destinationOrder - majesticGreenOrder);
+      speechText += ` and get down ${destinationDistance - 1} stations later.`;
+      var stationsFiltered = stations.filter(x => {
+        if (x.order ==  destinationCalculation && x.line == destinationLine[0].line) {
+          return true;
+        }
+        return false;
       });
-      // if(destinationOrder > 8){
-      //   stationsFiltered = stationsFiltered.sort(function(a,b){
-      //     return b.order - a.order;
-      //   });
-      // }
-      console.log(destinationOrder);
       console.log(stationsFiltered);
-      speechText += ` ${stationsFiltered[0].name} station`;
+      if(stationsFiltered.length > 0){
+        speechText += ` ${destinationStation} will come right after ${stationsFiltered[0].name} station`;
+      }
 
     }
-
+    
     return handlerInput.responseBuilder
       .speak(speechText)
-      .reprompt('Here we are')
-      .withSimpleCard('Hello World', speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
       .getResponse();
   },
 };
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    const speechText = 'I can help you with the route from one station to another. Just say your Origin station and your destination station to get started';
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
       .getResponse();
   },
 };
 
 const CancelAndStopIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent' ||
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
     const speechText = 'Goodbye!';
 
     return handlerInput.responseBuilder
       .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
       .getResponse();
   },
 };
@@ -166,7 +202,7 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
-    FareIntentHandler,
+    RouteIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
